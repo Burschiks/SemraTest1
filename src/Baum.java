c84674d652b27d577909bdc35ff3328761323c2f
burschik <burschik@hotmail.de>
2017-06-21 11:08:51 +0200

    Aufgaben erweitert 0.2

--- src/Baum.java
+++ src/Baum.java
@@ -6,6 +6,14 @@ public class Baum
 	//2. "count" soll Anzahl der Knoten des Baumes zurückgeben
 	//3. "schwellenArray" soll alle Knoten in einem Array aus Knoten zurückgeben die über einem übergeben Wert sind
 	//4. "schwellenListe" selbe wie 3 nur anstatt eines Array soll eine Liste zurückgegen werden
+	//5. "maxValue" soll den größten Wert als Ganzahl zurückgeben, der in dem Baum vorhanden ist
+	//6. "avg" soll den Schnitt in einem passendem Datentyp zurück geben
+	//7. "add" soll einen Teilbaum an den Baum hängen(auf passende Sortierung achten #Suchbaum)
+	//         Ein Teilbaum kann aus beliebig vielen Knoten bestehen aber mind. einem.
+	//			Doppelte Werte sollen dabei nicht einsortiert werden
+	//8. "cut" soll einen Teilbaum ab einem übergebenden Wert abgetrennt werden. Der abgetrennte Baum soll zurückgegeben werden
+	//Beispiel cut(3) -> führt dszu, dass die Methode cut als Rückgabe den Baum mit den Werten {-3,3,4} zurückgibt
+	// und der ursprüngliche Baum diese Werte nicht mehr enthält
 	
 	//Aufgaben
 	//Ergänzen Sie das Feld für die Wurzel
@@ -13,6 +21,7 @@ public class Baum
 	//							5
 	//					3 				8
 	//				-3 		4
+	//			-5 	  0
 	//Impelmentieren Sie diese Methoden, überlegen Sie sich dabei bitte den geeigneten Methodenkopf also
 	//Rückgabewert für diese Methoden.
 	//Bedenken Sie es gibt keine doofe Fragen :) der Lehrende steht ihnen zur Seite
